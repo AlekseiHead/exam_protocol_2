d8aff6f996f0b8959e6564885ebd9fa161b47278
Aleksei Golovanev <head.dev22@gmail.com>
2023-08-20 23:57:26 +0300

    add nodules array fields

--- src/components/ResultsFormComponent/ResultsFormComponent.tsx
+++ src/components/ResultsFormComponent/ResultsFormComponent.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createRoot } from "react-dom/client";
+//import { createRoot } from "react-dom/client";
 import { Formik, Field, Form, ErrorMessage, FieldArray } from "formik";
 
 const initialValues = {
@@ -8,6 +8,11 @@ const initialValues = {
     {
       lung_position: "",
       email: "",
+      local: "",
+      type_of_nodule: "",
+      size_of_finding: "",
+      volume: "",
+      benign_signs: "",
     },
   ],
 };
@@ -15,13 +20,13 @@ const initialValues = {
 const ProtocolComponent = () => {
   return (
     <div>
-      <h1>Очаг № 2</h1>
-      <img src="./src/assets/26.png" alt="22w2" />
+      <h1>Протокол обследования</h1>
       <Formik
         initialValues={initialValues}
         onSubmit={async (values) => {
           await new Promise((r) => setTimeout(r, 500));
           alert(JSON.stringify(values, null, 2));
+          console.log(JSON.stringify(values, null, 2));
         }}
       >
         {({ values }) => (
@@ -32,19 +37,10 @@ const ProtocolComponent = () => {
                   {values.nodules.length > 0 &&
                     values.nodules.map((nodule, index) => (
                       <div className="row" key={index}>
-                        <div className="col lung-position__choice">
+                        <div>Очаг № {index + 1} </div>
+                        <div className="col lung-position-select">
                           <label htmlFor={`nodules.${index}.lung_position`}>
-                            Расположение легкого{" "}
-                          </label>
-                          <Field
-                            name={`nodules.${index}.lung_position`}
-                            placeholder="Выберите расположение"
-                            type="text"
-                          />
-                        </div>
-                        <div className="col lung-position__choice">
-                          <label htmlFor={`nodules.${index}.lung_position`}>
-                            Расположение легкого{" "}
+                            Расположение легкого:{" "}
                           </label>
                           <Field
                             name={`nodules.${index}.lung_position`}
@@ -52,9 +48,77 @@ const ProtocolComponent = () => {
                             type="text"
                             as="select"
                           >
-                            <option value="red">Red</option>
-                            <option value="green">Green</option>
-                            <option value="blue">Blue</option>
+                            <option value="default"></option>
+                            <option value="Левое">Левое</option>
+                            <option value="Правое">Правое</option>
+                          </Field>
+                        </div>
+                        <div className="col local-input">
+                          <label htmlFor={`nodules.${index}.local`}>
+                            Локализация очага:{" "}
+                          </label>
+                          <Field
+                            name={`nodules.${index}.local`}
+                            placeholder="Укажите локализацию очага..."
+                            type="text"
+                          />
+                        </div>
+                        <div className="col type-of-nodule-select">
+                          <label htmlFor={`nodules.${index}.type_of_nodule`}>
+                            Тип очага:{" "}
+                          </label>
+                          <Field
+                            name={`nodules.${index}.type_of_nodule`}
+                            placeholder="Выберите расположение"
+                            type="text"
+                            as="select"
+                          >
+                            <option value="default"></option>
+                            <option value="Солидный">Солидный</option>
+                            <option value="Частично солидный">
+                              Частично солидный
+                            </option>
+                            <option value="«Матовое стекло»">
+                              «Матовое стекло»
+                            </option>
+                          </Field>
+                        </div>
+                        <div className="col size-of-finding-input">
+                          <label htmlFor={`nodules.${index}.size_of_finding`}>
+                            Размер очага в мм:{" "}
+                          </label>
+                          <Field
+                            name={`nodules.${index}.size_of_finding`}
+                            placeholder="Укажите размер очага..."
+                            type="text"
+                          />
+                        </div>
+                        <div className="col volume-input">
+                          <label htmlFor={`nodules.${index}.volume`}>
+                            Объём в мм<sup>3</sup>:{" "}
+                          </label>
+                          <Field
+                            name={`nodules.${index}.volume`}
+                            placeholder="Укажите объем очага..."
+                            type="text"
+                          />
+                        </div>
+                        <div className="col benign-signs-select">
+                          <label htmlFor={`nodules.${index}.benign_signs`}>
+                            Признаки доброкачественности:{" "}
+                          </label>
+                          <Field
+                            name={`nodules.${index}.benign_signs`}
+                            placeholder="Выберите признаки..."
+                            type="text"
+                            as="select"
+                          >
+                            <option value="Нет">Нет</option>
+                            <option value="Кальцинация">Кальцинация</option>
+                            <option value="Жир">Жир</option>
+                            <option value="Внутрилегочный лимфоузел">
+                              Внутрилегочный лимфоузел
+                            </option>
                           </Field>
                         </div>
                         <div className="col">
@@ -100,7 +164,5 @@ const ProtocolComponent = () => {
     </div>
   );
 };
-
-ReactDOM.render(<ProtocolComponent />, document.getElementById("root"));
 
 export default ProtocolComponent;
